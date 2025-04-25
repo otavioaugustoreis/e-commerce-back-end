9880b877d1cbb5521d4a5b60fe6452d8b7f57769
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-04-25 17:30:47 -0300

    feat: criando usuarioRepository

--- Cadastro.Data/Data/AppDbContext.cs
+++ Cadastro.Data/Data/AppDbContext.cs
@@ -10,7 +10,23 @@ namespace Cadastro.Data.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<PagamentoEntity> _Pagamento;
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
 
-    }
+        public DbSet<PagamentoEntity>? _Pagamento { get; set; }
+        public DbSet<UsuarioEntity>? _Usuario { get; set; }
+        public DbSet<PedidoItemEntity>? _PedidoItem { get; set; }
+        public DbSet<PedidoEntity>? _Pedido { get; set; }
+        public DbSet<ProdutoEntity>? _Produto { get; set; }
+        public DbSet<NotificacaoEntity>? _Notificacao { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext)
+            .Assembly);
+        }
+}
 }
---
+++ Cadastro.Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,53 @@
+﻿using Cadastro.Data.Data;
+using Cadastro.Data.Repositories.Pattern;
+using Cadastro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro.Data.Repositories
+{
+    public class UsuarioRepository : IRepository<UsuarioEntity>
+    {
+
+        private readonly AppDbContext appDbContext;
+
+        public UsuarioRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<UsuarioEntity> CreateAsync(UsuarioEntity entity)
+        {
+             appDbContext._Usuario.Add(entity);
+             return entity;
+        }
+
+        public async Task<IEnumerable<UsuarioEntity>> GetAsync()
+        {
+            return await appDbContext._Usuario.ToListAsync();
+        }
+
+        public async Task<UsuarioEntity> GetByIdAsync(int? id)
+        {
+            var usuario =  appDbContext._Usuario.FirstOrDefault(p => p.PkId == id);
+
+            return  usuario;
+        }
+        public async Task<UsuarioEntity> RemoveAsync(UsuarioEntity entity)
+        {
+
+             appDbContext._Usuario.Remove(entity);
+            return entity;
+        }
+
+        public async Task<UsuarioEntity> UpdateAsync(UsuarioEntity entity)
+        {
+           appDbContext.Update(entity);
+            return entity;
+        }
+    }
+}
